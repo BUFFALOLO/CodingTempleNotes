380eb244e16db0265e5b65be2d6f06aef882f232
Lauren Farrell <150389282+BUFFALOLO@users.noreply.github.com>
2024-09-16 18:47:07 +0000

    continued class

---
+++ Module2Lesson1Topic3Exercises.py
@@ -0,0 +1,44 @@
+# 1. Naming Convention Challenge: Create a variable that holds your full name.
+full_name = "Lauren Farrell"
+print(full_name)
+
+# 2. Case Sensitivity Quiz: Define two variables myName and myname. Assign your first name to myName and your last name to myname.
+myName = "Lauren"
+myname = "Farrell"
+print(myName, myname)
+
+# 3. Assignment Practice: Assign your favorite color to a variable called fav_color and print it.
+fav_color = "blue"
+print(fav_color)
+
+# 4. Multiple Values Assignment: Assign values "apple", "banana", "cherry" to variables fruit1, fruit2, and fruit3 in a single line.
+fruit1, fruit2, fruit3 = "apple", "banana", "cherry"
+print(fruit1, fruit2, fruit3)
+
+# 5. DataTypes Quest: Assign an integer, a float, and a string to threee different variables and print each variable using the type() function.
+number = 25
+decimalnumber = 2.53654
+setastring = "I love pizza"
+print(type(number))
+print(type(decimalnumber))
+print(type(setastring))
+
+# 6. The Variable's Idendity Card: For the variable mystery_variable = "1234", use type() function to determine its data type.
+mystery_variable = "1234"
+print(type(mystery_variable))
+
+# 7. Dynamic Typing Challenge: Assign a number to a variable named dynamicVar and the reassign a string value
+dynamicVar = 25
+print(dynamicVar)
+dynamicVar = "Today is Monday"
+print(dynamicVar)
+
+# 8. Keyword Examination: Write a schort script that tries to use if as a variable name.
+# if = "This will give me an error as you can't assign a variable to a Python keyword"
+# print(if)
+# SyntaxError: invalid syntax
+
+# 9. Constants Discovery: Define a constant named PI and assign it the value 3.1459.
+PI = 3.1459
+print(PI)
+
